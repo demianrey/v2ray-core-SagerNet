5e43ccc55af82b568f1ed767cdefc77a7b4ec29e
世界 <i@sekai.icu>
2022-01-07 16:15:11 +0800

    Skip send endpoint in mux if needed

--- common/mux/session.go
+++ common/mux/session.go
@@ -142,6 +142,8 @@ type Session struct {
 	parent       *SessionManager
 	ID           uint16
 	transferType protocol.TransferType
+	endpoint     net.Destination
+	sendEndpoint int
 }
 
 // Close closes all resources associated with this session.
